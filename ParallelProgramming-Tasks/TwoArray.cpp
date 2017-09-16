066cc24ff1354dbd6f31350acaceec8c952a3dab
BohdanRomaniuk <bohdan2307@gmail.com>
2017-09-17 01:03:54 +0300

    Update Two Array task with two thread using promise and future

--- ParallelProgramming-Tasks/TwoArray.cpp
+++ ParallelProgramming-Tasks/TwoArray.cpp
@@ -3,32 +3,50 @@
 #include <future>
 using namespace std;
 
-//Second thread
-void summing(promise<int*> arr1, int arr2[], unsigned length, int* result)
+//First thread
+void summing(promise<int*> arr1, int* arr2, unsigned length, int* sum)
 {
 	int* inner_arr = new int[length]{ 1,4,5,6,12 };
 	for (unsigned i = 0; i < length; ++i)
 	{
-		*result += inner_arr[i] + arr2[i];
+		*sum += inner_arr[i] + arr2[i];
 	}
 	arr1.set_value(inner_arr);
 }
 
-//First thread - Main Thread
+//Second thread
+void multiplying(int* arr1, int* arr2, unsigned length, promise<int*> mult)
+{
+	int* inner_mult = new int[length];
+	for (unsigned i = 0; i < length; ++i)
+	{
+		inner_mult[i] = arr1[i] * arr2[i];
+	}
+	mult.set_value(inner_mult);
+}
+
 void main()
 {
+	int size = 5;
 	int sum = 0;
-	int mult = 0;
+	int* mult;
+
 	promise<int*> arr1_promise;
 	future<int*> arr1_result = arr1_promise.get_future();
-	int* arr2 = new int[5]{ 3,7,8,9,10 };
-	thread second(summing, move(arr1_promise), arr2, 5, &sum);
-	int* arr1 = arr1_result.get();
-	for (unsigned i = 0; i < 5; ++i)
-	{
-		mult += arr1[i] * arr2[i];
-	}
 
+	int* arr2 = new int[5]{ 3,7,8,9,10 };
+	thread first(summing, move(arr1_promise), arr2, size, &sum);
+
+	promise<int*> mult_promise;
+	future<int*> mult_result = mult_promise.get_future();
+
+	int* arr1 = arr1_result.get();
+	thread second(multiplying, arr1, arr2, size, move(mult_promise));
+
+	if (first.joinable())
+	{
+		first.join();
+	}
 	if (second.joinable())
 	{
 		second.join();
@@ -43,6 +61,12 @@ void main()
 	{
 		cout << arr2[i] << " ";
 	}
-	cout << "\nSum=" << sum << "\tMult=" << mult << endl;
+	mult = mult_result.get();
+	cout << "\nSum=" << sum << "\nMult=";
+	for (int i = 0; i < size; ++i)
+	{
+		cout << mult[i] << " ";
+	}
+	cout << endl;
 	system("pause");
 }
